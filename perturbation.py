953385f251e717ae532ab903f046f44edf43ee7a
dlacksthd94 <dlacksthd94@naver.com>
2025-03-19 14:05:47 -0700

    add Proximity class

--- perturbation.py
+++ perturbation.py
@@ -8,24 +8,37 @@ import numpy as np
 import random
 import lpips
 import utils
+import pickle
 
 transform = transforms.Compose([transforms.ToTensor()])
 mnist_train = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 
 # sample images
-N_SAMPLE = 100
+N_SAMPLE = 1000
+random.seed(1234)
 subset_indices = random.choices(range(60000), k=N_SAMPLE)  # First N samples
 data = Subset(mnist_train, subset_indices)
 
-CF = utils.Perturbation(data)
+PTB = utils.Perturbation(data)
+
 ########## black patches (removing part of digits)
-result_black_patch = CF.apply_black_patches(stride=2, patch_width=5, patch_height=5) # output size: (1000 x #perturbed_images)
+stride = 2
+patch_black_width = 8
+patch_black_height = 8
+result_black_patch = PTB.apply_black_patches(stride, patch_black_width, patch_black_height) # output size: (1000 x #perturbed_images)
 image_original, images_black_patch, label = random.choice(result_black_patch)
 utils.save_gif(images_black_patch.squeeze(), file_name="mnist_black_patch.gif")
-utils.save_png(image_original, "iamge.png")
+utils.save_png(image_original, file_name="image.png")
+with open(f"data/test_black_patch_w{patch_black_width}h{patch_black_height}.pickle", 'wb') as f:
+    pickle.dump(result_black_patch, f)
 
-########## white patches (adding new part to digits)
-result_white_patch = CF.apply_white_patches(stride=2, patch_width=8, patch_height=4) # output size: (1000 x #perturbed_images)
+########## black patches (removing part of digits)
+stride = 2
+patch_white_width = 4
+patch_white_height = 4
+result_white_patch = PTB.apply_white_patches(stride, patch_white_width, patch_white_height) # output size: (1000 x #perturbed_images)
 image_original, images_white_patch, label = random.choice(result_white_patch)
 utils.save_gif(images_white_patch.squeeze(), file_name="mnist_white_patch.gif")
-utils.save_png(image_original, "iamge.png")
+utils.save_png(image_original, file_name="image.png")
+with open(f"data/test_white_patch_w{patch_white_width}h{patch_white_height}.pickle", 'wb') as f:
+    pickle.dump(result_white_patch, f)
--- utils.py
+++ utils.py
@@ -1,15 +1,17 @@
 import torch
-import torchvision
-from torchvision.transforms import v2 as transforms
 import torch.nn.functional as F
 from torch.utils.data import Subset
+import torchvision
+from torchvision.transforms import v2 as transforms
+from torchvision import models
+import lpips
+
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 import imageio
+
+from tqdm import tqdm
 import numpy as np
 import random
-import lpips
-import utils
 
 class Perturbation():
     def __init__(self, data):
@@ -21,7 +23,7 @@ class Perturbation():
         grid_height = torch.linspace(-1, 1, patch_height)
         D1, D2 = torch.meshgrid(grid_height, grid_width, indexing='ij')
         gaussian_weights = torch.exp(-(D1**2 + D2**2) / (2 * self.std**2))
-        noise = torch.randn((patch_height, patch_width)) * self.std * 0.1  # Adding small noise for randomness
+        noise = torch.randn((patch_height, patch_width)) * self.std * 0.05  # Adding small noise for randomness
         kernel = gaussian_weights + noise
         kernel = kernel - kernel.min()  # Normalize
         kernel = kernel / kernel.max()
@@ -52,7 +54,7 @@ class Perturbation():
         for y in range(0, image.shape[1] - (patch_height - 1), stride):
             for x in range(0, image.shape[2] - (patch_width - 1), stride):
                 patched_image = self._attach_black_patch(image, x, y, patch_width, patch_height)
-                if (image != patched_image).sum() > patch_height*patch_width/2:
+                if (image != patched_image).sum() > patch_height*patch_width/5: # kernel should overlap with at least 20% of digits
                 # if (image.bool() & (image != patched_image)).sum() > 0:
                     images_perturbed.append(patched_image)
         return images_perturbed
@@ -78,7 +80,7 @@ class Perturbation():
             except:
                 print("Error: Patch size is too big! Reduce patch size or change random seed")
                 return None, None
-            result.append((image, images_perturbed, label))
+            result.append(tuple([image, images_perturbed, label]))
         return result
     
     def apply_white_patches(self, stride, patch_width, patch_height):
@@ -93,11 +95,54 @@ class Perturbation():
             except:
                 print("Error: Patch size is too big! Reduce patch size or change random seed")
                 return None, None
-            result.append((image, images_perturbed, label))
+            result.append(tuple([image, images_perturbed, label]))
         return result
+
+class Proximity():
+    def __init__(self):
+        self.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+        self.model_LPIPS = None
     
-def save_png(sample, file_name):
-    plt.imsave(file_name, sample.squeeze(), cmap='gray')
+    def LPIPS(self, model, result):
+        self.model_LPIPS = lpips.LPIPS(net=model)
+        _ = self.model_LPIPS.to(self.device)
+
+        result_lpips = []
+        for image_original, images_perturbed, label in tqdm(result):
+            transform = transforms.Compose([
+                transforms.Resize(64),  # Resize images to a input size
+                transforms.ToTensor(),
+                # transforms.Normalize(mean=[0.5], std=[0.5])  # Normalize to [-1, 1]
+            ])
+            image_original = transform(image_original).to(self.device)
+            images_perturbed = transform(images_perturbed).to(self.device)
+            dist_lpips = self.model_LPIPS(image_original, images_perturbed).squeeze().cpu().detach()
+            result_lpips.append(tuple([image_original, images_perturbed, label, dist_lpips]))
+        return result_lpips
+    
+    def SimCLR(self, result):
+        model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        model.fc = torch.nn.Identity()  # Remove classification layer to get embeddings
+        model = model.to(self.device)
+        _ = model.eval()
+
+        result_simclr = []
+        for image_original, images_perturbed, label in tqdm(result):
+            transform = transforms.Compose([
+                # transforms.Resize(224),  # ResNet50 expects 224x224
+                transforms.RGB(),
+            ])
+            image_original = transform(image_original).to(self.device).unsqueeze(0)
+            images_perturbed = transform(images_perturbed).to(self.device)
+            with torch.no_grad():
+                feature_original = model(image_original)
+                features_perturbed = model(images_perturbed)
+            cos_sim = F.cosine_similarity(feature_original, features_perturbed)
+            result_simclr.append((image_original, images_perturbed, label, cos_sim))
+        return result_simclr
+
+def save_png(sample, file_name='image_png'):
+    transforms.ToPILImage()(sample).save('image.png')
 
 def save_gif(sample, file_name):
     imageio.mimsave(file_name, np.array(sample)*256, fps=5)
@@ -110,16 +155,25 @@ if __name__ == "__main__":
     subset_indices = random.choices(range(60000), k=10)  # First 1000 samples
     data = Subset(mnist_train, subset_indices)
 
-    CF = Perturbation(data)
-    self = CF
+    PTB = Perturbation(data)
+    self = PTB
     stride = 2
     patch_width = 4
     patch_height = 4
 
-    result_black_patch = CF.apply_black_patches(stride, patch_width, patch_height) # output size: (1000 x #perturbed_images)
+    result_black_patch = PTB.apply_black_patches(stride, patch_width, patch_height) # output size: (1000 x #perturbed_images)
     image_original, images_black_patch, label = random.choice(result_black_patch)
     save_gif(images_black_patch.squeeze(), file_name="mnist_black_patch.gif")
 
-    result_white_patch = CF.apply_white_patches(stride, patch_width, patch_height) # output size: (1000 x #perturbed_images)
+    result_white_patch = PTB.apply_white_patches(stride, patch_width, patch_height) # output size: (1000 x #perturbed_images)
     image_original, images_white_patch, label = random.choice(result_white_patch)
     save_gif(images_white_patch.squeeze(), file_name="mnist_white_patch.gif")
+
+    PRX = Proximity()
+    self = PRX
+    model = 'alex'
+    result = result_black_patch
+    PRX.LPIPS('alex', result_black_patch)
+    PRX.LPIPS('squeeze', result_black_patch)
+    PRX.LPIPS('vgg', result_black_patch)
+
